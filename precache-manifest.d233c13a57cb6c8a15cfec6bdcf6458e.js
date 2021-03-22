a7a7d71602cb71784a9c533747244d368398c6be
wutchara <SSTMUHAM@gmail.com>
2021-03-22 22:19:44 +0700

    Updates

---
+++ precache-manifest.d233c13a57cb6c8a15cfec6bdcf6458e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4695c21fd211d6a8ee7b044b708f154",
+    "url": "/react-pwa/index.html"
+  },
+  {
+    "revision": "8a362e1e7a19d8fb3efe",
+    "url": "/react-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "23175cb25103214482ef",
+    "url": "/react-pwa/static/css/main.723c8bc5.chunk.css"
+  },
+  {
+    "revision": "8a362e1e7a19d8fb3efe",
+    "url": "/react-pwa/static/js/2.436754dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pwa/static/js/2.436754dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23175cb25103214482ef",
+    "url": "/react-pwa/static/js/main.d41e4fc1.chunk.js"
+  },
+  {
+    "revision": "080bf034c760c5c36a1a",
+    "url": "/react-pwa/static/js/runtime-main.2c6b34b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
